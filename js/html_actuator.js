37c1cf9d157619e516e3b8c249f69a81e071d174
Enchantment Enhancement <116798346+enchantmentenhancement@users.noreply.github.com>
2025-06-14 16:36:58 +0100

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -57,7 +57,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  if (tile.value > 99) classes.push("tile-super");
+  if (tile.value > 999999999999999999999) classes.push("tile-super");
 
   this.applyClasses(wrapper, classes);
 
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "50, the one you need" : "How?";
+  var message = won ? "7 7 7 7 7 7 7" : "x";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
